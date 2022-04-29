9490640c58758402307229bd9e57b823a6d30833
Pascal Hartmann <pascal.hartmann@philasearch.com>
2022-04-29 09:49:14 +0200

    Save new clients in openvpn folder

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -95,7 +95,7 @@ new_client () {
 	echo "<tls-crypt>"
 	sed -ne '/BEGIN OpenVPN Static key/,$ p' /etc/openvpn/server/tc.key
 	echo "</tls-crypt>"
-	} > ~/"$client".ovpn
+	} > /etc/openvpn/client/"$client".ovpn
 }
 
 if [[ ! -e /etc/openvpn/server/server.conf ]]; then
@@ -438,7 +438,7 @@ verb 3" > /etc/openvpn/server/client-common.txt
 	echo
 	echo "Finished!"
 	echo
-	echo "The client configuration is available in:" ~/"$client.ovpn"
+	echo "The client configuration is available in:" /etc/openvpn/client/"$client.ovpn"
 	echo "New clients can be added by running this script again."
 else
 	clear
@@ -470,7 +470,7 @@ else
 			# Generates the custom client.ovpn
 			new_client
 			echo
-			echo "$client added. Configuration available in:" ~/"$client.ovpn"
+			echo "$client added. Configuration available in:" /etc/openvpn/client/"$client.ovpn"
 			exit
 		;;
 		2)
